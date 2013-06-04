33946c5537c0cf51677f3ea18f227a8d9416e7a5
Dverbrugge <don@mvdesigngroupinc.com>
2013-06-04 12:57:30 -0700

    removed box class

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 			$("#container > div").hover(function(){
-						$(this).toggleClass("banana");										
+			$(this).toggleClass("banana");										
 				});
-
 			$("#container > div").click(function(){
 					alert($(this).text());
 				});
